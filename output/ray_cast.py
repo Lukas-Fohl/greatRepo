f60ac7db2968cb4c02735147f0ba697158437b9b
Lukas Fohl <Lukas@fohl.net>
2022-07-13 19:48:06 +0200

    der Anfang vom Ende (raycasting)

--- output/ray_cast.py
+++ output/ray_cast.py
@@ -1,3 +1,4 @@
+from turtle import distance
 from output import screen_renderer as screen_render_import
 from output import output as output_import
 from designe import map as map_import
@@ -5,19 +6,46 @@ import math as math_import
 
 final_view: int = [0]
 cam_pos = [0,0]
+degrees_ = 0
 
 def raycasting(render_distacne_in: int,degrees_in: int,map_in: str = [[],[]]):
     #do the fun stuff
     global cam_pos
+    global  degrees_ 
+    degrees_ = degrees_in
     if output_import.cam_eq_player == True:
         cam_pos = map_import.get_current_player_position()
+    casting()
     screen_render_import.renderer(final_view)
     return
 
 def casting():
+    final_lenght = 0
     x_offset = 0
     y_offset = 0
+    try:
+        both_casting()
+    except:
+        return final_lenght
     return None
+
+def both_casting():
+    while get_horizontal_distance() > get_vertical_distance():
+        vertical_search()
+    while get_horizontal_distance() < get_vertical_distance():
+        horizonal_search()
+    both_casting()
+    return
+
+def vertical_search():
+    #manage the casting (vertical) 
+    get_vertical_distance()
+    return
+    
+def horizonal_search():
+    #manage the casting (horizontal) 
+    get_horizontal_distance()
+    return
 
 def vertical(angle_in: float,x_offset:int):
     hiting =  math_import.tan(deg_2_rad(angle_in))*x_offset
@@ -42,12 +70,22 @@ def orientation(angle: int):
     if angle >= 0:
         choice = 0
     elif angle >= 90:
-        choice = 0
+        choice = 1
     elif angle >= 180:
-        choice = 0
+        choice = 2
     elif angle >= 270:
-        choice = 0
+        choice = 3
     return choice
+
+def get_vertical_distance():
+    vertical()
+    return
+
+def get_horizontal_distance():
+    horizontal()
+    return
+
+######
 
 def output_just_cuz(map_in: str = [[],[]]):
     for y in map_in:
